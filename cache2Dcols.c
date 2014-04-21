95a2a391e473f667889db0dc7ea95c0a2bbe900f
Taylor Lee <taylorl@mumble-07.cs.wisc.edu>
2014-04-21 14:48:09 -0500

    Comments added to cache2Drows.c and cache2Dcols.c

--- cache2Dcols.c
+++ cache2Dcols.c
@@ -1,14 +1,24 @@
+/**
+ * Assignment 4 cache2Dcols.c
+ * By Taylor Lee, Section 2
+ * Partner: Joseph Kelley, Section 1
+ *
+ *
+ * This program will print out the time every second
+ */
 
-int arr[2000][1000];
+int twoDArray[2000][1000]; // The 2000 by 1000 2-dimensional array we use for this program.
+
 
 int main()
 {
-    int row,column;
+
+    int row,column; //The Row and Column indicies.
     for (column = 0; column < 1000; column++)
     {
         for (row = 0; row < 2000; row++)
         {
-            arr[row][column] = -3;
+            twoDArray[row][column] = -3;
         }
     }
     return 0;
--- cache2Drows.c
+++ cache2Drows.c
@@ -1,18 +1,26 @@
+/**
+ * Assignment 4 cache2Drows.c
+ * By Taylor Lee, Section 2
+ * Partner: Joseph Kelley, Section 1
+ *
+ *
+ * This program will print out the time every second
+ */
+
+int twoDArray[2000][1000]; // The 2000 by 1000 2-dimensional array we use for this program.
 
 
-int twoDArray[2000][1000];
+int main()
+{
 
-
-
-int main(){
-	int x,y;
-
-	for(x = 0; x < 2000; x++)
+	int row,column; //The Row and Column indicies.
+	for (row = 0; row < 2000; row++)
 	{
-		for(y=0; y < 1000; y++)
+		for (column=0; column < 1000; column++)
 		{
-			twoDArray[x][y] = (-3);
+			twoDArray[row][column] = -3;
 		}
 	}
 	return 0;
+
 }
